d2bafbd7ca6b94f20f798b3d6bc9f78ccbb7434d
jbadaj <eric5477@naver.com>
2024-12-29 19:31:19 +0900

    coffee main

--- Java_Study05/src/Coffee/Menu.java
+++ Java_Study05/src/Coffee/Menu.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
-    public static void main(String[] args) {
-        // 커피 리스트 생성
-        List<Coffee> coffeeList = new ArrayList<>();
-        coffeeList.add(new Coffee("Americano", 4000));
-        coffeeList.add(new Coffee("Latte", 4500));
-        coffeeList.add(new Coffee("Espresso", 3500));
-        coffeeList.add(new Coffee("Cappuccino", 5000));
-
-        // 메뉴판 출력
-        System.out.println("=== Coffee Menu ===");
-        for (Coffee coffee : coffeeList) {
-            System.out.println(coffee.getName() + " : " + coffee.getPrice() + "원");
-        }
-    }
+//    public static void main(String[] args) {
+//        // 커피 리스트 생성
+//        List<Coffee> coffeeList = new ArrayList<>();
+//        coffeeList.add(new Coffee("Americano", 4000));
+//        coffeeList.add(new Coffee("Latte", 4500));
+//        coffeeList.add(new Coffee("Espresso", 3500));
+//        coffeeList.add(new Coffee("Cappuccino", 5000));
+//
+//        // 메뉴판 출력
+//        System.out.println("=== Coffee Menu ===");
+//        for (Coffee coffee : coffeeList) {
+//            System.out.println(coffee.getName() + " : " + coffee.getPrice() + "원");
+//        }
+//    }
 }
