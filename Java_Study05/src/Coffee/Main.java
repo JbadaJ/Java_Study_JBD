59cc2c5a314947c0b039f7fd817b85a90d56bd55
jbadaj <eric5477@naver.com>
2025-01-02 23:01:03 +0900

    Receipt

--- Java_Study05/src/Coffee/Main.java
+++ Java_Study05/src/Coffee/Main.java
@@ -7,15 +7,19 @@ public class Main {
     public static void main(String[] args) {
         // 커피 리스트 생성
         List<Coffee> coffeeList = new ArrayList<>();
+        List<Receipt> orderList = new ArrayList<>();
         coffeeList.add(new Coffee("Americano", 4000));
         coffeeList.add(new Coffee("Latte", 4500));
         coffeeList.add(new Coffee("Espresso", 3500));
         coffeeList.add(new Coffee("Cappuccino", 5000));
-
+        orderList.add(new Receipt("first order", 8000));
         // 메뉴판 출력
         System.out.println("=== Coffee Menu ===");
         for (Coffee coffee : coffeeList) {
             System.out.println(coffee.getName() + " : " + coffee.getPrice() + "원");
         }
+        System.out.println("\n===Total Order===");
+        for (Receipt receipt : orderList) {
+        	System.out.println(receipt.getName() + ": " + receipt.getPrice() + "원");}
     }
 }
--- Java_Study05/src/Coffee/Receipt.java
+++ Java_Study05/src/Coffee/Receipt.java
@@ -1,5 +1,20 @@
 package Coffee;
 
 public class Receipt {
+    private String orderNumber;
+    private int total;
 
+    public Receipt(String orderNumber, int total) {
+        this.orderNumber = orderNumber;
+        this.total = total;
+    }
+
+    // Getter
+    public String getName() {
+        return orderNumber;
+    }
+
+    public int getPrice() {
+        return total;
+    }
 }
