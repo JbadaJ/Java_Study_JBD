07f753b1af65efd8ebae8fb63462480ebfb403e6
jbadaj <eric5477@naver.com>
2025-01-04 20:15:38 +0900

    receipt

--- Java_Study05/src/Coffee/Main.java
+++ Java_Study05/src/Coffee/Main.java
@@ -11,7 +11,10 @@ public class Main {
         menu.showMenu();     // 메뉴 출력
 
         int totalPrice = menu.cash(scanner); // 커피 주문 및 총합 계산
+
+        Receipt r1 = new Receipt("Order1",totalPrice);
         System.out.println("총 결제 금액: " + totalPrice + "원");
+        System.out.println("영수증: "+ r1.getName() +"- "+ r1.getPrice());
 
         scanner.close();
     }
